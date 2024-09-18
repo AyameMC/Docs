7c82207af89b1a505c47fbfee1dc7e4d8b4f6cec
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-09-18 21:33:16 +0900

    更新 index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
-import LogoSvg from './logo.svg';
+import LogoSvg from 'static/img/logo.svg';
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
