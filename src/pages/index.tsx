fb739cce042fcd958d04f0b87372c1edceef916b
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-09-18 21:20:08 +0900

    更新 index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-// import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
@@ -34,9 +34,9 @@ export default function Home(): JSX.Element {
       title={`主页`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      // <main>
-       //  <HomepageFeatures />
-      // </main>
+       <main>
+         <HomepageFeatures />
+       </main>
     </Layout>
   );
 }
