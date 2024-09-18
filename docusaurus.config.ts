c63f812c4db4a36f9e98b75c7622dedf0c069388
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-09-18 19:53:04 +0900

    更新 docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/AyameMC/Docs/tree/main',
         },
         blog: {
           showReadingTime: false,
@@ -49,7 +49,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/AyameMC/Docs/tree/main/docs',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
