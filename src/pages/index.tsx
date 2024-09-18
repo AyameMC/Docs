c7ee1df122d04b4f68fdfeba0f1fee80dfab6b2b
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-09-18 21:47:19 +0900

    更新 index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <LogoSvg />
+        <LogoSvg className={styles.logo} />
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
