25e56ce76c8358a9a6a0637633faeec1ca54f1d6
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-09-19 21:30:09 +0900

    css r

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+
+
+
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -6,12 +9,10 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 import LogoSvg from '/static/img/logo.svg';
-
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
-  
   return (
-    <header className={clsx('hero hero--primary', styles.hero)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <LogoSvg className={styles.logo} />
         <Heading as="h1" className="hero__title" style={{ color: 'var(--ifm-hero-text-color)' }}>
@@ -32,15 +33,14 @@ function HomepageHeader() {
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
-  
   return (
     <Layout
       title={`主页`}
       description="Description will go into a meta tag in <head />">
-        <HomepageHeader />
-        {/* <main>
-          { <HomepageFeatures /> }
-        </main> */}
+      <HomepageHeader />
+       {/* <main>
+         { <HomepageFeatures /> }
+       </main> */}
     </Layout>
   );
 }
