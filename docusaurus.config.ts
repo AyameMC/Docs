3c94dfd75de4bec61ce375428cadbd0a5f6bc5d0
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-09-23 22:45:34 +0900

    更新 docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -105,7 +105,7 @@ const config: Config = {
     },
     prism: {
       theme: prismThemes.github,
-      darkTheme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
       additionalLanguages: ['java', 'groovy', 'gradle'],
     },
   },
